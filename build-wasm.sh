e2f56f497501fc400fe5ab9ba588ad65863ad11f
pmp-p <mail.peny@free.fr>
2024-03-02 12:30:06 +0100

    fix long double to IEEE_QUAD_LE

--- build-wasm.sh
+++ build-wasm.sh
@@ -16,44 +16,34 @@ export PYBUILD=${PYBUILD:-3.13}
 
 PY=${SDKROOT}/python3-wasm
 
-# multi sdk case
-if [ -f /pp ]
-then
- $PY -m pip install --force https://github.com/cython/cython/releases/download/3.0.8/Cython-3.0.8-py2.py3-none-any.whl
-fi
-
-echo $HPY
-$HPY -m pip install --force ninja
-# /opt/python-wasm-sdk/devices/x86_64/usr/bin/ninja
-which ninja
-
 git restore .
+git pull
 
 patch -p1 <<END
 diff --git a/numpy/_core/meson.build b/numpy/_core/meson.build
-index 113adb5..4cda911 100644
+index 113adb5f7..0e9db9bfb 100644
 --- a/numpy/_core/meson.build
 +++ b/numpy/_core/meson.build
-@@ -478,7 +478,8 @@ int main(void) {
-   ''').stdout()
- endif
- if longdouble_format == 'UNKNOWN' or longdouble_format == 'UNDEFINED'
--  error('Unknown long double format of size: ' + cc.sizeof('long double').to_string())
-+  warning('Unknown long double format of size: ' + cc.sizeof('long double').to_string())
-+  longdouble_format = 'IEEE_DOUBLE_LE'
- endif
- cdata.set10('HAVE_LDOUBLE_' + longdouble_format, true)
-
+@@ -394,7 +394,7 @@ endforeach
+ # https://github.com/numpy/numpy/blob/eead09a3d02c09374942cdc787c0b5e4fe9e7472/numpy/core/setup_common.py#L264-L434
+ # This port is in service of solving gh-23972
+ # as well as https://github.com/mesonbuild/meson/issues/11068
+-longdouble_format = meson.get_external_property('longdouble_format', 'UNKNOWN')
++longdouble_format = 'IEEE_QUAD_LE'
+ if longdouble_format == 'UNKNOWN'
+   longdouble_format = meson.get_compiler('c').run(
+ '''
 END
 
-
-
-CC=emcc \
- CXX=em++ \
- EMCC_CFLAGS="-I/opt/python-wasm-sdk/devices/emsdk/usr/include/python${PYBUILD}" \
+echo "MESON=$(which meson)"
+CC=emcc CXX=em++ \
+ EMCC_CFLAGS="-I${SDKROOT}/devices/emsdk/usr/include/python${PYBUILD}" \
  $PY -m build \
  -Csetup-args="-Ddisable-optimization=true" \
  -Csetup-args="-Ddisable-threading=true" \
+ -Csetup-args="-Dmkl-threading=false" \
+ -Csetup-args="-Dallow-noblas=true" \
+ -Csetup-args="--cross-file=/data/git/numpy-wasm/meson-python-cross-file.ini" \
  --no-isolation .
 
 # -Csetup-args="-Ddisable-highway=true" \
